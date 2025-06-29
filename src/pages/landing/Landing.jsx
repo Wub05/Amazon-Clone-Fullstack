7ddd61926cd4b5f4060542f7579cbdf96d2a7570
Wubishet <wubishet05@gmail.com>
2025-06-30 02:56:08 +0300

    Update Landing.jsx

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Carousel from "../../components/Carousel";
 import Category from "../../components/category/Category";
 import Product from "../../components/product/Product";
-import Layout from "../../components/Layout/Layout";
+import Layout from "../../components/layout/Layout";
 
 const Landing = () => {
   return (
