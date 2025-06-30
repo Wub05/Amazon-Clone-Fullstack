a48281c68716f6b4de6878aeec9205e4d69b5542
Wubishet <wubishet05@gmail.com>
2025-06-30 03:49:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Amazon-Clone-Fullstack/",
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
 });
